cdfa0be58b9998d2cb664e8710c97d806aa3a899
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-10-26 21:39:44 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header'
+import Video from './components/Video'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Video />
+    </>
+  )
+}
+
+export default App;
